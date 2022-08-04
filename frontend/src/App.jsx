3e3ea0ec9a507e5ff7cffd9dfc137829fa08e4c6
asharokhin <aleks.sharokhin@gmail.com>
2022-08-04 18:02:38 +0300

    Add empty frontend React project with dependencies. Add gitignore.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppNavigation from './components/AppNavigation';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavigation />
+      English study App
+
+    </div>
+  );
+}
+
+export default App;
